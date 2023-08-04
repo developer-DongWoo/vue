bb37b7e1e5cf944ffe3b5d696e7fb3bd3f75ec6a
developer-DongWoo <ehddn8547@gmail.com>
2023-08-05 00:25:09 +0900

    feat/vue2/v1

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import Signup from "@/views/Signup.vue";
+import ForgotPassword from "@/views/ForgotPassword.vue";
+import BotConsole from "@/views/BotConsole.vue";
+import BotOrder from "@/views/BotOrder.vue";
+import MyPage from "@/views/Mypage.vue";
+import Payment from "@/views/Payment.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/login", component: Login },
+  { path: "/signup", component: Signup },
+  { path: "/forgot-password", component: ForgotPassword },
+  { path: "/bot-console", component: BotConsole },
+  { path: "/bot-order", component: BotOrder },
+  { path: "/my-page", component: MyPage },
+  { path: "/plan-purchase", component: Payment },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
